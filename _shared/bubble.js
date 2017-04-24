7a22acea0e1aa14ab710f579bf24ebe5ca80b914
leny <info@flatland.be>
2017-04-24 12:54:44 +0200

    Implement exo-four

--- _shared/bubble.js
+++ _shared/bubble.js
@@ -1,1 +1,39 @@
 // hepl-mmi/meet-canvas-animation - Bubble
+
+class Bubble {
+    constructor( { context, width, height } ) {
+        let iGreen = Math.floor( Math.random() * 128 ),
+            iBlue = Math.floor( Math.random() * 128 ) + 128;
+
+        this.context = context;
+        this.canvasWidth = width;
+        this.canvasHeight = height;
+        this.radius = Math.random() * 20 + 10;
+        this.color = `rgba( 0, ${ iGreen }, ${ iBlue }, ${ this.radius / 30 } )`;
+        this.speed = this.radius / 10;
+        this.x = Math.floor( Math.random() * width );
+        this.y = height + this.radius;
+    }
+
+    draw() {
+        let { context, color, x, y, radius } = this;
+
+        context.fillStyle = color;
+        context.beginPath();
+        context.arc( x, y, radius, 0, Math.PI * 2 );
+        context.closePath();
+        context.fill();
+    }
+
+    animate() {
+        this.y -= this.speed;
+        if ( this.y < this.radius * -1 ) {
+            this.x = Math.floor( Math.random() * this.canvasWidth );
+            this.y = this.canvasHeight + this.radius;
+        }
+
+        this.draw();
+    }
+}
+
+window.Bubble = Bubble;
--- exo-four/script.js
+++ exo-four/script.js
@@ -1,1 +1,31 @@
 // hepl-mmi/meet-canvas-animation - exo-four
+
+let oApp;
+
+class ExoFourApp extends CanvApp {
+    setup() {
+        let i = 0;
+
+        while ( i++ < this.bubblesCount ) {
+            this.bubbles.push( new Bubble( this ) );
+        }
+
+        this.animate();
+    }
+
+    animate() {
+        let { context, width, height } = this;
+
+        context.clearRect( 0, 0, width, height );
+        this.bubbles.forEach( ( oBubble ) => {
+            oBubble.animate();
+        } );
+
+        window.requestAnimationFrame( this.animate.bind( this ) );
+    }
+}
+
+oApp = new ExoFourApp( "app" );
+oApp.bubbles = [];
+oApp.bubblesCount = 50 + Math.round( Math.random() * 50 );
+oApp.setup();
